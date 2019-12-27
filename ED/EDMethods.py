0ddaffbd3054fcb5394b9cfdaa79b4becd87fcad
Alexandru Petrescu <xander@Alexandrus-MacBook-Pro.local>
2019-12-27 14:16:00 +0200

    Added New ED Method

--- ED/EDMethods.py
+++ ED/EDMethods.py
@@ -1,11 +1,11 @@
-from wordcloud import WordCloud
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.decomposition import LatentDirichletAllocation as LDA
 from nltk.tokenize import word_tokenize, sent_tokenize
+from datetime import datetime, timedelta
 from nltk.stem import WordNetLemmatizer
 from scipy.signal import find_peaks
 from nltk.corpus import stopwords
-from datetime import datetime, timedelta
+from wordcloud import WordCloud
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -14,6 +14,7 @@ import numpy as np
 import warnings
 import string
 import nltk
+import math
 import csv
 import re
 import os
@@ -24,16 +25,28 @@ sns.set_style('whitegrid')
 wordnet_lemmatizer = WordNetLemmatizer()
 default_stopwords = stopwords.words('english') # or any other list of your choice
 
-DATASET_PATH = os.path.join('DataSample',  'S140SampleED.csv')
-CLEANED_DATASET_PATH = os.path.join('DataSample',  'S140SampleEDCleaned.csv')
-TOPIC_FILE = os.path.join('DataSample',  'S140SampleEDTopics.csv')
+SAMPLE_DATASET_PATH = os.path.join('DataSample',  'S140SampleED.csv')
+SAMPLE_CLEANED_DATASET_PATH = os.path.join('DataSample',  'S140SampleEDCleaned.csv')
+SAMPLE_TOPIC_FILE = os.path.join('DataSample',  'S140SampleEDTopics.csv')
 
+DATASET_PATH = os.path.join('DataSample',  'S140ED.csv')
+CLEANED_DATASET_PATH = os.path.join('DataSample',  'S140EDCleaned.csv')
 S140_ED_FILE = os.path.join('Datasets', 'S140ED.csv')
+S140_CO_ED_FILE = os.path.join('Datasets', 'S140EDC0.csv')
 
 class EDMethod(object):    
-    def __init__(self, numberOfTopics = 10, numberOfWords = 5, datasetPath = DATASET_PATH, cleanedDatasetPath = CLEANED_DATASET_PATH):
+    def __init__(self, 
+                methodName,
+                numberOfTopics = 10, 
+                numberOfWords = 5, 
+                datasetPath = DATASET_PATH, 
+                cleanedDatasetPath = CLEANED_DATASET_PATH):
+        self.methodName = methodName
+        self.TopicFile = '[' + methodName + ']S140EDTopics.csv'
+
         self.dataset = datasetPath
         self.cleanedDataset = cleanedDatasetPath
+
         self.numberOfTopics = numberOfTopics
         self.numberOfWords = numberOfWords
         
@@ -62,106 +75,21 @@ class EDMethod(object):
 
         return text
 
-    def readData(self, datasetPath = CLEANED_DATASET_PATH):
+    def readData(self):
         data = None
 
         try:
-            data = pd.read_csv(datasetPath, engine='python', encoding='utf-8', error_bad_lines=False)
+            data = pd.read_csv(self.dataset, engine='python', encoding='utf-8', error_bad_lines=False)
         except:
-            data = pd.read_csv(DATASET_PATH, engine='python', encoding='utf-8', error_bad_lines=False)
+            data = pd.read_csv(self.dataset, engine='python', encoding='utf-8', error_bad_lines=False)
             data['text'] = data['text'].map(lambda x: self.cleanText(x))
 
-            data.to_csv(datasetPath, sep=',', encoding='utf-8', index=False)
+            data.to_csv(self.cleanedDataset, sep=',', encoding='utf-8', index=False)
 
         return data
 
-    def plotMost10CommonWords(self, count_data, count_vectorizer):
-        words = count_vectorizer.get_feature_names()
-        total_counts = np.zeros(len(words))
-
-        for t in count_data:
-            total_counts+=t.toarray()[0]
-        
-        count_dict = (zip(words, total_counts))
-        count_dict = sorted(count_dict, key=lambda x:x[1], reverse=True)[0:10]
-        words = [w[0] for w in count_dict]
-        counts = [w[1] for w in count_dict]
-        x_pos = np.arange(len(words)) 
-        
-        figure = plt.figure(2, figsize=(15, 15/1.6180))
-        plt.subplot(title='10 most common words')
-        sns.set_context("notebook", font_scale=1.25, rc={"lines.linewidth": 2.5})
-        sns.barplot(x_pos, counts, palette='husl')
-        plt.xticks(x_pos, words, rotation=90) 
-        plt.xlabel('words')
-        plt.ylabel('counts')
-
-        figure.savefig('[OLDA]Sentiment140SampleCommon.png')
-
-    def saveTopics(self, model, count_vectorizer, n_top_words):
-        words = count_vectorizer.get_feature_names()
-        data_labels = []
-
-        for _, topic in enumerate(model.components_):
-            data_labels.append(" ".join([words[i] for i in topic.argsort()[:-n_top_words - 1:-1]]))
-
-        topics = pd.DataFrame(data=data_labels, columns=['topic'])
-        topics.to_csv(TOPIC_FILE, sep=',', encoding='utf-8', index=False)
-        
-        return topics
-
-    def run(self):
-        None
-
-class OLDA(EDMethod):
-    def trainOLDA(self):
-        print('Gathering Data...')
-        papers = self.readData()
-
-        # Join the different processed titles together.
-        long_string = ','.join(list(str(x) for x in papers['text'].values))
-
-        # Create a WordCloud object
-        wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='steelblue')
-
-        # Generate a word cloud
-        cloud = wordcloud.generate(long_string)
-        cloud.to_file('[OLDA]CLOUD_Sentiment140Sample.png')
-        print('Generated WordCloud')
-
-        # Initialise the count vectorizer
-        count_vectorizer = CountVectorizer()
-
-        # Fit and transform the processed titles
-        count_data = count_vectorizer.fit_transform(papers['text'].values.astype('str'))
-
-        # Visualise the 10 most common words
-        self.plotMost10CommonWords(count_data, count_vectorizer)
-        print('Plotted Most Common Words')
-
-        # Create and fit the LDA model
-        lda = LDA(n_components=self.numberOfTopics)
-        lda.fit(count_data)
-
-        print('OLDA is done')
-        # Print the topics found by the LDA model
-        return self.saveTopics(lda, count_vectorizer, self.numberOfWords), papers
-
-    def run(self):
-        topics = None
-        papers = None
-
-        try:
-            topics = pd.read_csv(TOPIC_FILE, engine='python', encoding='utf-8', error_bad_lines=False)
-            papers = self.readData()
-
-            print('OLDA: Loaded Topics and Dataset')
-        except:
-            print('OLDA: Training...')
-            topics, papers = self.trainOLDA()
-
-            print('OLDA: Trained')
-
+    def getStats(self, papers, topics):
+        papers.columns = papers.columns.str.lower()
         topics['set'] = topics['topic'].map(lambda x: set(x.split()))
 
         magnitude = {}
@@ -199,9 +127,9 @@ class OLDA(EDMethod):
                     if given_date > event_end[original_topic]:
                         event_end[original_topic] = given_date
 
-        print('Writing Output')
+        print(self.methodName + ': Writing Output')
 
-        with open('[OLDA]TopicResults.csv', mode='w') as csv_out_file:
+        with open('[Stats]' + self.TopicFile, mode='w') as csv_out_file:
             csv_writer = csv.writer(csv_out_file, delimiter=',')
             
             for _, row in topics.iterrows():
@@ -212,7 +140,110 @@ class OLDA(EDMethod):
                 '\'' + str(event_end.get(topic, 'NULL')) +'\'',
                 '\'' + str(topic) +'\''])
 
+    def train(self):
+        None
+
+    def run(self):
+        topics = None
+        papers = None
+
+        try:
+            topics = pd.read_csv(self.TopicFile, engine='python', encoding='utf-8', error_bad_lines=False)
+            papers = self.readData()
+
+            print(self.methodName + ': Loaded Topics and Dataset')
+        except:
+            print(self.methodName + ': Training...')
+            topics, papers = self.train()
+
+            print(self.methodName + ': Trained')
+
+        self.getStats(papers, topics)
+
+class OLDA(EDMethod):
+    def __init__(self, 
+                numberOfTopics = 10, 
+                numberOfWords = 5, 
+                datasetPath = SAMPLE_DATASET_PATH, 
+                cleanedDatasetPath = SAMPLE_CLEANED_DATASET_PATH):
+        EDMethod.__init__(self, 'OLDA', numberOfTopics, numberOfWords,datasetPath,  cleanedDatasetPath)
+
+    def plotMost10CommonWords(self, count_data, count_vectorizer):
+        words = count_vectorizer.get_feature_names()
+        total_counts = np.zeros(len(words))
+
+        for t in count_data:
+            total_counts+=t.toarray()[0]
+        
+        count_dict = (zip(words, total_counts))
+        count_dict = sorted(count_dict, key=lambda x:x[1], reverse=True)[0:10]
+        words = [w[0] for w in count_dict]
+        counts = [w[1] for w in count_dict]
+        x_pos = np.arange(len(words)) 
+        
+        figure = plt.figure(2, figsize=(15, 15/1.6180))
+        plt.subplot(title='10 most common words')
+        sns.set_context("notebook", font_scale=1.25, rc={"lines.linewidth": 2.5})
+        sns.barplot(x_pos, counts, palette='husl')
+        plt.xticks(x_pos, words, rotation=90) 
+        plt.xlabel('words')
+        plt.ylabel('counts')
+
+        figure.savefig('[OLDA]Sentiment140SampleCommon.png')
+
+    def saveTopics(self, model, count_vectorizer, n_top_words):
+        words = count_vectorizer.get_feature_names()
+        data_labels = []
+
+        for _, topic in enumerate(model.components_):
+            data_labels.append(" ".join([words[i] for i in topic.argsort()[:-n_top_words - 1:-1]]))
+
+        topics = pd.DataFrame(data=data_labels, columns=['topic'])
+        topics.to_csv(self.TopicFile, sep=',', encoding='utf-8', index=False)
+        
+        return topics
+
+    def train(self):
+        print('Gathering Data...')
+        papers = self.readData()
+
+        # Join the different processed titles together.
+        long_string = ','.join(list(str(x) for x in papers['text'].values))
+
+        # Create a WordCloud object
+        wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='steelblue')
+
+        # Generate a word cloud
+        cloud = wordcloud.generate(long_string)
+        cloud.to_file('[OLDA]CLOUD_Sentiment140Sample.png')
+        print('Generated WordCloud')
+
+        # Initialise the count vectorizer
+        count_vectorizer = CountVectorizer()
+
+        # Fit and transform the processed titles
+        count_data = count_vectorizer.fit_transform(papers['text'].values.astype('str'))
+
+        # Visualise the 10 most common words
+        self.plotMost10CommonWords(count_data, count_vectorizer)
+        print('Plotted Most Common Words')
+
+        # Create and fit the LDA model
+        lda = LDA(n_components=self.numberOfTopics)
+        lda.fit(count_data)
+
+        print('OLDA is done')
+        # Print the topics found by the LDA model
+        return self.saveTopics(lda, count_vectorizer, self.numberOfWords), papers
+
 class MABED(EDMethod):
+    def __init__(self, 
+                numberOfTopics = 10, 
+                numberOfWords = 5, 
+                datasetPath = SAMPLE_DATASET_PATH, 
+                cleanedDatasetPath = SAMPLE_CLEANED_DATASET_PATH):
+        EDMethod.__init__(self, 'MABED', numberOfTopics, numberOfWords,datasetPath,  cleanedDatasetPath)
+
     def run(self):
         print('Gathering Data...')
         self.readData()
@@ -228,7 +259,15 @@ class MABED(EDMethod):
 
         os.chdir(currentDir)
         
-class PeakDetection(EDMethod):
+class PeakDetection(EDMethod):    
+    def __init__(self, 
+                numberOfTopics = 10, 
+                numberOfWords = 5, 
+                datasetPath = SAMPLE_DATASET_PATH, 
+                cleanedDatasetPath = SAMPLE_CLEANED_DATASET_PATH):
+        EDMethod.__init__(self, 'PeakDetection', numberOfTopics, numberOfWords,datasetPath,  cleanedDatasetPath)
+        self.timeSplit = 100
+
     def getHoursBetween(self, diff):
         days, seconds = diff.days, diff.seconds
         hours = days * 24 + seconds // 3600
@@ -249,60 +288,166 @@ class PeakDetection(EDMethod):
 
         diff = maxDate - minDate
 
-        split = int(self.getHoursBetween(diff) / binCount)
-
+        split = int(math.ceil(self.getHoursBetween(diff) / binCount))
         bins = {}
 
-        for i in range(binCount - 1):
-            lDate = minDate + timedelta(hours=(i * split))
-            uDate = minDate + timedelta(hours=((i + 1) * split))
+        if (split == 0):
+            currentBin = []
 
-            bins[(lDate, uDate)] = []
-        bins[(minDate + timedelta(hours=((binCount - 1) * split)), maxDate)] = []
+            for _, row in tweets.iterrows():
+                currentBin.append((row['Date'], row['text']))
 
-        for _, row in tweets.iterrows():
-            currentDate = row['Date']
-            diff = currentDate - minDate
+            bins[(minDate, maxDate)] = currentBin
+        else:
+            for i in range(binCount - 1):
+                lDate = minDate + timedelta(hours=(i * split))
+                uDate = minDate + timedelta(hours=((i + 1) * split))
 
-            binIndex = 0 if (split == 0) else int(self.getHoursBetween(diff)/split)
+                bins[(lDate, uDate)] = []
 
-            lDate = minDate + timedelta(hours=(binIndex * split))
-            uDate = minDate + timedelta(hours=((binIndex + 1) * split))
+            bins[(minDate + timedelta(hours=((binCount - 1) * split)), maxDate)] = []
 
-            bins[(lDate, uDate)].append((currentDate, row['text']))
+            for _, row in tweets.iterrows():
+                currentDate = row['Date']
+                diff = currentDate - minDate
+
+                binIndex = 0 if (split == 0) else int(self.getHoursBetween(diff)/split)
+
+                lDate = minDate + timedelta(hours=(binIndex * split))
+                uDate = minDate + timedelta(hours=((binIndex + 1) * split))
+
+                bins[(lDate, uDate)].append((currentDate, row['text']))
 
         return bins
 
-    def run(self):
+    def saveTopics(self, popularTopics): 
+        topics = pd.DataFrame(data=popularTopics, columns=['topic'])
+        topics.to_csv(self.TopicFile, sep=',', encoding='utf-8', index=False)
+        
+        print(self.methodName + ': Saved Topics...')
+
+        return topics
+
+    def train(self):
         print('Running PeakDetection...')
         print('Gathering Data...')
-        self.dataset = self.readData(datasetPath= S140_ED_FILE).head(100)
+        self.dataset = self.readData()
 
         self.dataset['Date'] = self.dataset['Date'].map(lambda x: datetime.strptime(x, "%Y-%m-%d-%H-%M-%S"))
+        self.dataset = self.dataset.sort_values('Date')
         print('Gathered Data...')
         
         bins = self.generateBins(self.numberOfTopics)
         print('Generated Bins...')
 
+        print('Getting Topics...')
+        topics = []
         for (interval, listOfTweets) in  bins.items():
-            (lowD, highD) = (interval[0] - interval[1])
+            maxIndex = len(listOfTweets)
+            if maxIndex == 0:
+                continue
+
+            (lowD, highD) = interval
+            lowS = lowD.strftime("%Y-%m-%d-%H-%M-%S")
+            highS = highD.strftime("%Y-%m-%d-%H-%M-%S")
+            print('Bin for: ' + lowS + ' to ' + highS)
+            
             diff = highD - lowD
             minutesBetween = self.getMinutesBetween(diff)
-            increment = float(minutesBetween / 100)
+            increment = float(minutesBetween / self.timeSplit)
+            index = 0
 
-            dates = [date for (date, tweet) in listOfTweets]
-            peaks, _ = find_peaks(dates)
+            left = lowD
+            right = lowD + timedelta(minutes=increment)
 
-            None
+            stats = {}
 
+            for i in range(self.timeSplit):
+                if index >= maxIndex:
+                    break
+                while index < maxIndex:
+                    (currentDate, _) = listOfTweets[index]
+                    index += 1
+
+                    if (left <= currentDate) and (currentDate <= right):
+                        stats[i] = stats.get(i, 0) + 1
+                    else:
+                        left = right
+                        right += timedelta(minutes=increment)
+                        break
+
+            lastIndex = maxIndex - 1
+            while index < maxIndex:
+                (currentDate, _) = listOfTweets[index]
+                index += 1
+
+                stats[lastIndex] = stats.get(lastIndex, 0) + 1
+             
+            distribution = list(stats.values())
+            peaks, _ = find_peaks(distribution)
+
+            if len(peaks) == 0:
+                continue
+
+            popularTweets = []
+            for peak in peaks:
+                (initialDate, tweet) = listOfTweets[peak]
+                popularTweets.append(tweet)
+
+                left = peak - 1
+                right = peak + 1
+
+                while left > 0:
+                    (currentDate, tweet) = listOfTweets[left]
+                    left -= 1
+
+                    diff = initialDate - currentDate
+                    minutesBetween = self.getMinutesBetween(diff)
+
+                    if minutesBetween > increment:
+                        break
+
+                    popularTweets.append(tweet)
+
+                while right < maxIndex:
+                    (currentDate, tweet) = listOfTweets[right]
+                    right += 1
+
+                    diff = initialDate - currentDate
+                    minutesBetween = self.getMinutesBetween(diff)
+
+                    if minutesBetween > increment:
+                        break
+
+                    popularTweets.append(tweet)
+            
+            tfidf = TfidfVectorizer(stop_words=default_stopwords)
+            response = tfidf.fit_transform(popularTweets)
+
+            feature_array = np.array(tfidf.get_feature_names())
+            tfidf_sorting = np.argsort(response.toarray()).flatten()[::-1]
+
+            top_n = feature_array[tfidf_sorting[:self.numberOfWords]]
+
+            topics.append(' '.join(top_n.tolist()))
+
+        return self.saveTopics(topics), self.dataset
 
 def sampleRun():
-    mabed_sample = MABED(numberOfTopics=10, numberOfWords=5)
-    mabed_sample.run()
+    MABEDSample = MABED()
+    OLDASample = OLDA()
+    PDSample = MABED()
 
-    olda_sample = OLDA(numberOfTopics=10, numberOfWords=5)
-    olda_sample.run()
+    for method in [MABEDSample, OLDASample, PDSample]:
+        method.run()
+
+def trueRun():
+    peakDetection = PeakDetection(datasetPath=S140_ED_FILE,
+                                cleanedDatasetPath=S140_ED_FILE)
+
+    for method in [peakDetection]:
+        method.run()
+
 
 if __name__ == '__main__':
-    peakDetection = PeakDetection(numberOfTopics=10, numberOfWords=5)
-    peakDetection.run()
+    trueRun()
